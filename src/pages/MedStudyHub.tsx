22e113acf7a4616f20ee062b924cdddc48d59f91
rizkyazhar486 <rizkyazhar486@gmail.com>
2026-09-07 06:02:44 +0700

    feat(study): add question-first Medical Library workflow

--- src/pages/MedStudyHub.tsx
+++ src/pages/MedStudyHub.tsx
@@ -1,11 +1,23 @@
+import { useSearchParams } from 'react-router-dom'
+import { MedicalLibraryWorkbench } from '../components/MedicalLibraryWorkbench'
 import { StudyCommandCenter } from '../components/StudyCommandCenter'
 import { MedStudyHub as MedStudyHubBase } from './MedStudyHubBase'
 
 export function MedStudyHub() {
+  const [params, setParams] = useSearchParams()
+
+  function runEvidenceQuery(query: string) {
+    const next = new URLSearchParams(params)
+    next.set('bagian', 'evidence')
+    next.set('cari', query)
+    setParams(next, { replace: true })
+  }
+
   return (
     <div className="space-y-5 pb-8">
       <StudyCommandCenter />
-      <MedStudyHubBase />
+      <MedicalLibraryWorkbench onRun={runEvidenceQuery} />
+      <MedStudyHubBase key={params.toString()} />
     </div>
   )
 }
