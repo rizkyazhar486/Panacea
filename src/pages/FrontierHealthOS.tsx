fcaae3267cef89a49a48084754559ae6eaf62c0b
rizkyazhar486 <rizkyazhar486@gmail.com>
2026-09-13 17:15:24 +0700

    feat(discovery): add falsification-first Discovery workbench
    * feat(discovery): add fail-closed workbench model
    
    * feat(discovery): add interactive falsification workbench
    
    * test(discovery): guard falsification-first workbench
    
    * feat(discovery): expose workbench in Frontier Health OS

--- src/pages/FrontierHealthOS.tsx
+++ src/pages/FrontierHealthOS.tsx
@@ -1,4 +1,5 @@
 import { useMemo, useState } from 'react'
+import { DiscoveryWorkbench } from '../components/frontier/DiscoveryWorkbench'
 import { PanaceaFrontierSeven } from '../components/frontier/PanaceaFrontierSeven'
 import { PanaceaHopeStack } from '../components/frontier/PanaceaHopeStack'
 import {
@@ -73,6 +74,7 @@ export function FrontierHealthOS() {
 
       <PanaceaFrontierSeven />
       <PanaceaHopeStack />
+      <DiscoveryWorkbench />
 
       <section className="space-y-4 rounded-[30px] border border-neutral-200 bg-white/70 p-4 dark:border-white/10 dark:bg-white/[.025] sm:p-5">
         <div>
