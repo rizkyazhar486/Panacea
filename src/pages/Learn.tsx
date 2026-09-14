0a55a922d0c54a3a7b483a50baeb630bacfd4d1b
rizkyazhar486 <rizkyazhar486@gmail.com>
2026-09-14 16:49:30 +0700

    feat(learn): make Learn the canonical unified workspace

--- src/pages/Learn.tsx
+++ src/pages/Learn.tsx
@@ -1,13 +1,7 @@
-import { LifeLibraryWorkbench } from '../components/LifeLibraryWorkbench'
-import { Learn as LearnBase } from './LearnBase'
+import { UnifiedLearnWorkspace } from './UnifiedLearnWorkspace'
 
 export function Learn() {
-  return (
-    <div className="space-y-6 pb-8">
-      <LifeLibraryWorkbench />
-      <LearnBase />
-    </div>
-  )
+  return <UnifiedLearnWorkspace />
 }
 
 export default Learn
