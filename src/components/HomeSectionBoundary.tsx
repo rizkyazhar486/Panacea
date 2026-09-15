6216a5fa9cf0cdb58225e207c8abe9546f5f9f24
rizkyazhar486 <rizkyazhar486@gmail.com>
2026-09-15 11:58:14 +0700

    fix(home): activate no-gray dark surface protocol [deploy-vercel]

--- src/components/HomeSectionBoundary.tsx
+++ src/components/HomeSectionBoundary.tsx
@@ -2,6 +2,7 @@ import { Component, type ReactNode } from 'react'
 import { HomeLiveWidgetRail } from './HomeLiveWidgetRail'
 import { HomeWidgetUniverse } from './HomeWidgetUniverse'
 import '../styles/home-mobile-stability.css'
+import '../styles/home-no-gray.css'
 
 type Props = {
   children: ReactNode
