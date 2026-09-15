358891c878606b187bdd9f2e312712235993b67b
rizkyazhar486 <rizkyazhar486@gmail.com>
2026-09-15 20:10:51 +0700

    chore(frontier): restore rational-drug-design PR scope

--- src/pages/FrontierHealthOS.tsx
+++ src/pages/FrontierHealthOS.tsx
@@ -1,6 +1,5 @@
 import { useMemo, useState } from 'react'
 import { DiscoveryWorkbench } from '../components/frontier/DiscoveryWorkbench'
-import { FutureSignalsRadar } from '../components/frontier/FutureSignalsRadar'
 import { RationalDrugDesignWorkbench } from '../components/frontier/RationalDrugDesignWorkbench'
 import { PanaceaFrontierSeven } from '../components/frontier/PanaceaFrontierSeven'
 import { PanaceaHopeStack } from '../components/frontier/PanaceaHopeStack'
@@ -77,7 +76,6 @@ export function FrontierHealthOS() {
       <PanaceaFrontierSeven />
       <PanaceaHopeStack />
       <DiscoveryWorkbench />
-      <FutureSignalsRadar />
       <RationalDrugDesignWorkbench />
 
       <section className="space-y-4 rounded-[30px] border border-neutral-200 bg-white/70 p-4 dark:border-white/10 dark:bg-white/[.025] sm:p-5">
