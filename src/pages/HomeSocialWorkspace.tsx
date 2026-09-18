dc091b870ffb3ed5bdc1b6a1d3f43023019d1d9b
rizkyazhar486 <rizkyazhar486@gmail.com>
2026-09-18 15:48:17 +0700

    fix(home): restore live fun widget rail to Home

--- src/pages/HomeSocialWorkspace.tsx
+++ src/pages/HomeSocialWorkspace.tsx
@@ -1,6 +1,7 @@
 import { lazy, Suspense, type ComponentType } from 'react'
 import { Link, useSearchParams } from 'react-router-dom'
 import { HomeCommandDeck } from '../components/HomeCommandDeck'
+import { HomeFunWidgetRail } from '../components/HomeFunWidgetRail'
 import { HomeHealthBrief } from '../components/HomeHealthBrief'
 import { HomeVisualLanding } from '../components/HomeVisualLanding'
 import { RelWidgetRumah } from '../components/RelWidgetRumah'
@@ -71,6 +72,7 @@ export function HomeSocialWorkspace() {
       {activeKey === 'home' ? (
         <div className="panacea-human-home">
           <HomeHealthBrief />
+          <HomeFunWidgetRail />
           <RelWidgetRumah />
           <HomeVisualLanding />
           <HomeCommandDeck />
